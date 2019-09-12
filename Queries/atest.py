ec9db63cf1e736ff8c8f55473d66153589aed129
mat <mat@mignin.com>
2019-09-12 10:24:58 -0400

    "$1"

---
+++ Queries/atest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import openpyxl
+
+wb1 = openpyxl.load_workbook('RawMaterial.xlsx')
+ws1 = wb1.active
+ws2 = wb1.create_sheet("unique2")
+
+values = []
+for i in range(2,ws1.max_row+1):
+    if ws1.cell(row=i,column=1).value in values:
+        pass
+    else:
+        values.append(ws1.cell(row=i,column=1).value)
+
+
+for value in values:
+    ws2.append([value])
+
+wb1.save('RawMaterial.xlsx')
