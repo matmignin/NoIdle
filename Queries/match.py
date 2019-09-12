96e869c6999305468c94ff39330f27bd90c68f61
mat <mat@mignin.com>
2019-09-12 11:54:11 -0400

    add match.py

---
+++ Queries/match.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+df = pd.read_excel('AllandEverythingFull.xlsx')
+df.drop_duplicates(['MATCODE'], keep='first', inplace=True)
+
+df.to_excel("AllandEverythingNew.xlsx", sheet_name='unique')
