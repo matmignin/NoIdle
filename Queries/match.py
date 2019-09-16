067fa36b897d11c5ead19fb43d1a4941e0650cd4
mat <mat@mignin.com>
2019-09-16 04:22:48 -0400

    made 2019 bl sheet

--- Queries/match.py
+++ Queries/match.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-df1 = pd.read_excel('RecentProductsNew.xlsx', sheet_name='unique Bulk Liquid')
+df1 = pd.read_excel('BulkLiquid19.xlsx', sheet_name='BL2019')
 df2 = pd.read_excel('MATnameTYPE.xlsx', sheet_name='MatNameType')
 
 merged_df = df2.merge(df1, how = 'inner', on = ['MATCODE'])
 
-merged_df.to_excel("RecentProductsMerge.xlsx", sheet_name='BLmerge')
+merged_df.to_excel("RecentBulkProducts.xlsx", sheet_name='BL2019')
 
