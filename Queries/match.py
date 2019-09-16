774b2631a3ea74aefc01e187b3f9344e89251058
mat <mat@mignin.com>
2019-09-16 06:08:01 -0400

    add final All_Products list

--- Queries/match.py
+++ Queries/match.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-df1 = pd.read_excel('BulkLiquid19.xlsx', sheet_name='BL2019')
+df1 = pd.read_excel('ACTIVE_ITEM.xlsx')
 df2 = pd.read_excel('MATnameTYPE.xlsx', sheet_name='MatNameType')
 
 merged_df = df2.merge(df1, how = 'inner', on = ['MATCODE'])
 
-merged_df.to_excel("RecentBulkProducts.xlsx", sheet_name='BL2019')
+merged_df.to_excel("All_Products.xlsx", sheet_name='All')
 
