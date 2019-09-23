7e988f8ad0850dcd9759af4867d55345bc52fbaf
matmignin <mat@mignin.com>
2019-09-23 09:01:57 -0400

    add features to tesd.py

--- Queries/test.py
+++ Queries/test.py
@@ -9,11 +9,15 @@ from click import command, option, argument
 @argument('sheet1')
 @argument('column1')
 def merge_sheets(book1, sheet1):
-    Data = pd.read_excel('ProductSpec.xlsx', sheet_name='JJ')
+    Data = pd.read_excel('ProductSpec.xlsx', sheet_name='JJ', parse_cols=0, index_col=0)
+    Data.columns = ['MatCodes´]
     df = pd.DataFrame(Data, columns= [column1])
     df.to_excel('test.xlsx')
+    df.loc(
 
 
 if __name__== '__main__':
     merge_sheets()
+
+process.extractOne("typo",choices=state_to_code.keys(),score_cutoff=80)
 
