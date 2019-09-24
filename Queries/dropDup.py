1682df4d9d01813b1e150f1f7af8ccd04b976e10
mat <mat@mignin.com>
2019-09-24 04:51:11 -0400

    organized

--- Queries/dropDup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-
-df = pd.read_excel('RecentProducts.xlsx', sheet_name='Bulk Liquid')
-df.drop_duplicates(['MATCODE'], keep='first', inplace=True)
-
-df.to_excel("RecentProductsNew.xlsx", sheet_name='unique')
-
