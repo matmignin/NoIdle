fa6dcdee8e258f9b385534b9384d44594797088a
matmignin <mat@mignin.com>
2019-10-09 05:24:08 -0400

    get drop duplicate ready for rm

--- scripts/dropDup.py
+++ scripts/dropDup.py
@@ -2,12 +2,17 @@
 
 import pandas as pd
 
-workbook1 = "BulkTests"
-sheet1 = "Bulkseparated"
-column1 = "TM"
-column2 = "PRODUCT_SPEC.COMPONENT"
-saveAs = "DropDuplicates"
+#workbook1 = "BulkTests"
+#sheet1 = "Bulkseparated"
+#column1 = "TM"
+#column2 = "PRODUCT_SPEC.COMPONENT"
+#saveAs = "DropDuplicates"
 
+workbook1 = "productSpecs"
+sheet1 = "rm Product specs (comp)"
+column1 = "testname"
+column2 = "PRODUCT_SPEC.COMPONENT"
+saveAs = "rm_componentTests"
 
 df = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 df.drop_duplicates([column1, column2], keep='first', inplace=True)
