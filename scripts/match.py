50133146d341ba2bd38da8204c4523c1ed94cee8
matmignin <mat@mignin.com>
2019-10-09 04:55:55 -0400

    improve match.py

--- scripts/match.py
+++ scripts/match.py
@@ -2,12 +2,23 @@
 
 import pandas as pd
 
-df2 = pd.read_excel('DuplicatesDeleted.xlsx', sheet_name='fgSpec')
-df1 = pd.read_excel('DuplicatesDeleted.xlsx', sheet_name='FG')
-x = "outer"
+workbook1 = ""
+sheet1 = ""
+match1 = ""
+
+workbook2 = ""
+sheet2 = ""
+match2 = ""
+
+mode = "outer"
+
+saveAs = ""
+
+df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
+df2 = pd.read_excel(workbook2 + ".xlsx", sheet_name=sheet2)
 
 
-merged_df = df2.merge(df1, how = x, left_on ='PRODUCT_CODE', right_on='MatCode', indicator=True)
+merged_df = df1.merge(df2, how = mode, left_on =match1, right_on=match2, indicator=True)
 
-merged_df.to_excel(x + ".xlsx", sheet_name="fgSpec" + x)
+merged_df.to_excel(saveAs + ".xlsx")
 
