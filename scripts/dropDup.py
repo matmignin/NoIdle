823cb4a6c729dd02114e5b80ec2b60231536c36a
mat <mat@mignin.com>
2019-10-11 19:21:50 -0400

    v

--- scripts/dropDup.py
+++ scripts/dropDup.py
@@ -8,14 +8,14 @@ import pandas as pd
 #column2 = "PRODUCT_SPEC.COMPONENT"
 #saveAs = "DropDuplicates"
 
-workbook1 = "productSpecs"
-sheet1 = "RMspecsComp"
-column1 = "TEST_NAME"
-column2 = "PRODUCT_SPEC.COMPONENT"
-saveAs = "rm_componentTests"
+workbook1 = "Master"
+sheet1 = "Convert"
+column1 = "METHOD"
+# column2 = "PRODUCT_SPEC.COMPONENT"
+# saveAs = "rm_componentTests"
 
 df = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
-df.drop_duplicates([column1, column2], keep='first', inplace=True)
+df.drop_duplicates(column1, keep='first', inplace=True)
 
-df.to_excel(saveAs + ".xlsx")
+df.to_excel("dupDrop" + sheet1 + ".xlsx")
 
--- scripts/match.py
+++ scripts/match.py
@@ -2,23 +2,23 @@
 
 import pandas as pd
 
-workbook1 = ""
-sheet1 = ""
-match1 = ""
+workbook1 = "Master"
+sheet1 = "EQTYP"
+match1 = "METHOD.CONVERT"
 
-workbook2 = ""
-sheet2 = ""
-match2 = ""
+workbook2 = "BulkTests" 
+sheet2 = "BulkAnalysis"
+match2 = "Name"
 
 mode = "outer"
 
-saveAs = ""
+# saveAs = workbook1 +"
 
 df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 df2 = pd.read_excel(workbook2 + ".xlsx", sheet_name=sheet2)
 
 
-merged_df = df1.merge(df2, how = mode, left_on =match1, right_on=match2, indicator=True)
+merged_df = df2.merge(df1, how = mode, left_on =match1, right_on=match2, indicator=True)
 
-merged_df.to_excel(saveAs + ".xlsx")
+merged_df.to_excel(sheet1 + "AND"  + sheet2 + ".xlsx", sheet_name=sheet1 + sheet2)
 
