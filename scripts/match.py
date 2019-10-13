355740482292448033a7bcb9d8f4938541c5ddc7
mat <mat@mignin.com>
2019-10-13 04:08:08 -0400

    update

--- scripts/match.py
+++ scripts/match.py
@@ -14,8 +14,8 @@ mode = "outer"
 
 # saveAs = workbook1 +"
 
-df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 df2 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet2)
+df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 
 
 merged_dfO = df1.merge(df2, how ='outer', left_on =match1, right_on=match2, indicator=True)
@@ -25,5 +25,6 @@ merged_dfL = df1.merge(df2, how ='left', left_on =match1, right_on=match2, indic
 
 merged_dfO.to_excel("Results/outer" + ".xlsx", sheet_name=sheet1 + sheet2)
 merged_dfI.to_excel("Results/inner.xlsx", sheet_name="inner")
+
 merged_dfR.to_excel("Results/right.xlsx", sheet_name=sheet1 + sheet2)
 merged_dfL.to_excel("Results/left.xlsx", sheet_name=sheet1 + sheet2)
