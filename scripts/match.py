1e3e715483ca6d2be2c520a56c58c52f466986d1
mat <mat@mignin.com>
2019-10-13 03:52:19 -0400

    update

--- scripts/match.py
+++ scripts/match.py
@@ -2,21 +2,28 @@
 
 import pandas as pd
 
-workbook1 = "ChemTests"
-sheet1 = "Combo"
-match1 = "METHOD"
+workbook1 = "Master"
+sheet1 = "Equipment"
+match1 = "EQMETHOD.CONVERT"
 
-workbook2 = workbook1 
-sheet2 = "ConvertMethod"
-match2 = "METHOD"
+# workbook2 = workbook1
+sheet2 = "TMdes"
+match2 = "TMMETHOD"
 
-mode = "left"
+mode = "outer"
+
+# saveAs = workbook1 +"
 
 df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 df2 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet2)
 
 
-merged_df = df2.merge(df1, how = mode, left_on=match1, right_on=match2, indicator=True)
+merged_dfO = df1.merge(df2, how ='outer', left_on =match1, right_on=match2, indicator=True)
+merged_dfI = df1.merge(df2, how = "inner", left_on =match1, right_on=match2, indicator=True)
+merged_dfR = df1.merge(df2, how ='right', left_on =match1, right_on=match2, indicator=True)
+merged_dfL = df1.merge(df2, how ='left', left_on =match1, right_on=match2, indicator=True)
 
-merged_df.to_excel(sheet1 + "AND"  + sheet2 + ".xlsx", sheet_name=sheet1 + sheet2)
-
+merged_dfO.to_excel("Results/outer" + ".xlsx", sheet_name=sheet1 + sheet2)
+merged_dfI.to_excel("Results/inner.xlsx", sheet_name="inner")
+merged_dfR.to_excel("Results/right.xlsx", sheet_name=sheet1 + sheet2)
+merged_dfL.to_excel("Results/left.xlsx", sheet_name=sheet1 + sheet2)
