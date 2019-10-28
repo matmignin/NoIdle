d2ebcf422eca40df330b1629132748263dab87ca
mat <mat@mignin.com>
2019-10-28 18:50:03 -0400

    bulk workbook almost  done

--- Data/scripts/BLmatch.py
+++ Data/scripts/BLmatch.py
@@ -2,21 +2,19 @@
 
 import pandas as pd
 
-workbook1 = "xBL_PRODUCT_CODE"
-sheet1 = "PLAN"
-sheet2 = "ANALYSIS"
-match1 = "PRODUCT_CODE"
+workbook1 = "../uqBL"
+sheet1 = "PLANS"
+sheet2 = "activeBLspecs"
+match = "PLANS"
 
-match2 = match1
+mode = "left"
 
-mode = "outer"
-
-saveAs = "BLx" + sheet2
+saveAs = "BLresultL"
 
 df1 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet1)
 df2 = pd.read_excel(workbook1 + ".xlsx", sheet_name=sheet2)
 
 
-merged_df = df1.merge(df2, how = mode, left_on =match1, right_on=match2, indicator=True)
+merged_df = df1.merge(df2, how = mode, left_on =match, right_on=match, indicator=True)
 
-merged_df.to_excel(saveAs + ".xlsx", sheet_name=saveAs)
+merged_df.to_excel("../results/" + saveAs + ".xlsx", sheet_name=saveAs)
